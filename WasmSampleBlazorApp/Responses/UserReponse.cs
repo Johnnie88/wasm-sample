57e23e3b40f9119a4ade09542d14ff0888d5eada
João Ricardo Baptista <jricardo.baptista@live.com>
2023-03-03 22:51:29 -0300

    feat: add UserReponse

---
+++ WasmSampleBlazorApp/Responses/UserReponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WasmSampleBlazorApp.Responses
+{
+    public class UserReponse
+    {
+        public Guid? Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
